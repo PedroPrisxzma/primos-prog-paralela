00346ca92583c919f25acdaa745b74ea0c3031df
Pedro Nascimento <28676630+PedroPrisxzma@users.noreply.github.com>
2019-08-22 13:53:11 -0300

    deleted unused file

--- auxiliar.h
+++
@@ -1,5 +0,0 @@
-// the definition of a struct used in the problem
-typedef struct {
-  int value; // indicates it's value, from [2, N]
-  int prime; // indicates if it is or not a prime number
-}Number;
